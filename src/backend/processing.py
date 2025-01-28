2c2c83545aed25b4af674cbc3a4dcf033c0307fb
NicolasLepki <nicolaslepki@gmail.com>
2025-01-28 15:25:02 -0500

    Expanded file structure, added templates for server.py in the backend, index.html and standard.css in the frontend. processing.py change is added notes.

--- src/backend/processing.py
+++ src/backend/processing.py
@@ -1,6 +1,8 @@
 """
 The idea of this file is to contain the algorithm that processes images into ascii and returns to server.
 Algorithm concept: image -> image data -> resize image -> grayscale -> map values to ascii -> return
+
+Note: will have to make this file importable, perhaps create a class structure instead of this plan
 """
 import PIL.Image
 
