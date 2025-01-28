7704bcddd595d3a7c5d68ec8a473c3c2e870f07f
NicolasLepki <nicolaslepki@gmail.com>
2025-01-28 13:20:17 -0500

    File structure, includes src code with backend and frontend directories, processing.py includes planned algorithm using pseudocode

---
+++ src/backend/processing.py
@@ -0,0 +1,12 @@
+"""
+The idea of this file is to contain the algorithm that processes images into ascii and returns to server.
+Algorithm concept: image -> image data -> resize image -> grayscale -> map values to ascii -> return
+"""
+import PIL.Image
+
+# function: converts to numpy array, grayscales, resizes
+# notes: should have max and min file sizes and error clauses
+
+# function: maps pixel values to ascii
+
+# function: format the ascii in json
